599f93846841005c77706017a541a873bc2bf89b
FauziMulyanaas <151744789+FauziMulyanaas@users.noreply.github.com>
2023-11-22 20:56:45 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,12 +7,12 @@ const users = require("./router/users");
 const category = require("./router/category");
 const auth = require("./router/auth");
 
-const corsOptions = {
-  origin: 'http://localhost:5173', 
-  optionsSuccessStatus: 200,
-};
-
-app.use(cors(corsOptions));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 
 
 app.use(express.urlencoded({ extended: false }));
